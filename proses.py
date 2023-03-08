6b6417e771cfc2eff2fb60790190503abc11b565
Yufri Luttu <yufridonluttu@gmail.com>
2023-03-08 12:08:14 +0800

    update pertama

--- proses.py
+++ proses.py
@@ -1,20 +1,22 @@
 from PIL import Image
 
-# Buka citra RGB
+# upload file gambar
 rgb_image = Image.open('mawar.jpg')
 
-# Konversi citra RGB ke grayscale
+# Konversi citra RGB => grayscale
 grayscale_image = rgb_image.convert('L')
 
-# Konversi citra grayscale ke biner
+# Konversi citra grayscale => biner
 threshold = 128
-binary_image = grayscale_image.point(lambda x: 0 if x < threshold else 255, '1')
+binary_image = grayscale_image.point(
+    lambda x: 0 if x < threshold else 255, '1')
 
-# Penskalaan citra
+# Penskalaan
 scale = 0.5
-scaled_image = binary_image.resize((int(binary_image.width*scale), int(binary_image.height*scale)))
+scaled_image = binary_image.resize(
+    (int(binary_image.width*scale), int(binary_image.height*scale)))
 
 # Simpan citra hasil pengolahan
-grayscale_image.save('grayscale_image.jpg')
-binary_image.save('binary_image.jpg')
-scaled_image.save('scaled_image.jpg')
+grayscale_image.save('gambar_grayscale.jpg')
+binary_image.save('gambar_binary.jpg')
+scaled_image.save('gambar_scaled.jpg')
